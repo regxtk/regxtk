966e0d4e8859a97902c2feae5fa85171b6993f55
regxtk <dazai.ru@mail.ru>
2024-04-11 00:55:36 +0300

    попукала

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <p className="abzac">
+            я Машка какашка
+      </p>
+    </main>
+  );
+}
